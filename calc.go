88f1448762fb9e7ecd7bb647ea6aa7a4a98787ba
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-17 13:17:12 +0100

    subtraction

--- calc.go
+++ calc.go
@@ -12,8 +12,18 @@ import (
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
 	number1, _ := strconv.Atoi(r.FormValue("number1"))
 	number2, _ := strconv.Atoi(r.FormValue("number2"))
+	operation := r.FormValue("operation")
+
+
+	var result int
+	if operation == "-" {
+		result = number1 - number2
+	} else {
+		result = number1 + number2	
+	}	
+
     fmt.Fprintf(w, "<h1>Result: %v</h1>",
-        number1 + number2)
+        result )
 }
 
 func main() {
