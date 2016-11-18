ceffc02230880128ec8101cc324ffd1d4438e70c
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-18 11:38:31 +0100

    reformat

--- calc.go
+++ calc.go
@@ -1,29 +1,25 @@
 package main
 
 import (
-	"net/http"
 	"fmt"
+	"net/http"
 	"strconv"
 )
-
-
-
 
 func calculateHandler(w http.ResponseWriter, r *http.Request) {
 	number1, _ := strconv.Atoi(r.FormValue("number1"))
 	number2, _ := strconv.Atoi(r.FormValue("number2"))
 	operation := r.FormValue("operation")
 
-
 	var result int
 	if operation == "-" {
 		result = number1 - number2
 	} else {
-		result = number1 + number2	
-	}	
+		result = number1 + number2
+	}
 
-    fmt.Fprintf(w, "<h1>Result: %v</h1>",
-        result )
+	fmt.Fprintf(w, "<h1>Result: %v</h1>",
+		result)
 }
 
 func main() {
