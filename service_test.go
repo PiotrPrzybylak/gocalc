6cc853e6a7c9c56e06f6eaac4d61c18fa37a0e34
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-21 10:42:37 +0100

    more tests

--- service_test.go
+++ service_test.go
@@ -2,10 +2,29 @@ package main
 
 import "testing"
 
+type testpair struct {
+  input string
+  result string
+}
+
+var tests = []testpair {
+ { "A", "a"},
+ { "b", "b"},
+ { "ŁódŹ", "łódź"},
+}
+
 func TestLowercase(t *testing.T) {
-  	svc := stringService{}
-    result, _ := svc.Lowercase("aBc");
-    if(result != "abc") {
-      t.Error("Expected abc got ", result);
+
+  svc := stringService{}
+
+  for _, pair := range tests {
+    result, _ := svc.Lowercase(pair.input);
+    if(result != pair.result) {
+      t.Error("For", pair.input,
+        "expected", pair.result,
+        "got", result);
     }
+  }
+
+
 }
