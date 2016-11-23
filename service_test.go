f04024c4778e156b94f258c45a5a4dda705aa40a
Piotr Przybylak <piotr.przybylak@rndity.com>
2016-11-23 11:53:25 +0100

    formatting

--- service_test.go
+++ service_test.go
@@ -3,28 +3,27 @@ package main
 import "testing"
 
 type testpair struct {
-  input string
-  result string
+	input  string
+	result string
 }
 
-var tests = []testpair {
- { "A", "a"},
- { "b", "b"},
- { "ŁódŹ", "łódź"},
+var tests = []testpair{
+	{"A", "a"},
+	{"b", "b"},
+	{"ŁódŹ", "łódź"},
 }
 
 func TestLowercase(t *testing.T) {
 
-  svc := stringService{}
+	svc := stringService{}
 
-  for _, pair := range tests {
-    result, _ := svc.Lowercase(pair.input);
-    if(result != pair.result) {
-      t.Error("For", pair.input,
-        "expected", pair.result,
-        "got", result);
-    }
-  }
-
+	for _, pair := range tests {
+		result, _ := svc.Lowercase(pair.input)
+		if result != pair.result {
+			t.Error("For", pair.input,
+				"expected", pair.result,
+				"got", result)
+		}
+	}
 
 }
